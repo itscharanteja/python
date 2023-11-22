021b7e7b4eaf2f1e6737ad29a422e72269c94378
Charan Teja <68527259+itscharanteja@users.noreply.github.com>
2023-11-22 21:28:54 +0530

    Hello Python

--- class.py
+++ class.py
@@ -1,34 +1,23 @@
-class cla1:
-    def __init__(self,id,name):
-        print("New object is being created..!")
-        self.id = id
-        self.name = name
-        self.follower = 0
+class User:
+    def __init__(self,user_id,username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
         self.following = 0
-        
-
-
 
     def follow(self,user):
-        user.follower += 1
-        user.following += 1
-        
-user1 =  cla1(501,'charan')
-user2 = cla1(502,'groot')
-user2.follow(user1)
-user2.follow(user1)
+        user.followers +=1
+        self.following +=1
 
-# a.user = "Charan"
-# a.id = "502"
-# print(a.id,'\n',a.name)
-# print(a.follower)
-# b = cla1()
-print(user1.follower)
-print(user1.following)
-print(user2.follower)
-print(user2.following)
 
-# b.user = "groot"
-# b.id = "503"
 
-# print(b.id)
+
+user1 = User(2, "itscharanteja")
+
+user2 = User(1, "itsarthurmorg")
+
+
+user1.follow(user2)
+
+print(user2.following, user2.followers)
+print(user1.following, user1.followers)
