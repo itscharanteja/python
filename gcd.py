3a5b5c92a985d2345cf33773702e61e5f2094ce4
Charan Sri Teja <68527259+itscharanteja@users.noreply.github.com>
2024-07-22 21:11:00 +0530

    .

--- gcd.py
+++ gcd.py
@@ -1,9 +1,13 @@
 def gcd(a,b):
     while b!=0:
+        
         t = a
         a = b
         b= t%b
         
     return a
 
-print(gcd(4,20))
+
+print(gcd(9,12))
+
+
--- linearsearch.py
+++ linearsearch.py
@@ -1,12 +1,10 @@
-def search(arr,x):
+def search_and_count(x,arr):
+    count = 0
     for i in range(len(arr)):
-        if arr[i]==x:
-            return i
-    return -1
-a = [1,2,34,5,6,7,3,4,2,8,9,5,55,65,453,343]
-s = search(a,242)
+        if arr[i] == x:
+            count+=1
+    return count
 
-if s == -1:
-    print("No value present in the list")
-else:
-    print(s)
+print(search_and_count(2,[2,2,2,2,2]))
+print(search_and_count(2,[2,2,2,2,2]))
+print(search_and_count(2,[2,2,2,2,2]))
